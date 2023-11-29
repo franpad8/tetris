31e62bc36477dbb5734a0da37ddf88581b18d5f1
Franklin Padilla <fpadilla@buk.cl>
2023-11-29 11:48:20 -0300

    init canvas

--- game.js
+++ game.js
@@ -1,5 +1,17 @@
-export default class Game {
+const WIDTH = 15
+const HEIGHT = 30
+const PIXELS_PER_SQUARE = 30
 
+export default class Game {
   init () {
+    const canvas = document.getElementById('canvas')
+    const context = canvas.getContext('2d')
+    context.fillStyle = '#000'
+    context.rect(0, 0, WIDTH * PIXELS_PER_SQUARE, HEIGHT * PIXELS_PER_SQUARE)
+    context.fill()
+  }
+
+  start () {
+
   }
 }
